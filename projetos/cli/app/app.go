7bcf9176822fd1d1fed062bb41822c349e8d9759
Diego Brito <diego.lima@sptech.school>
2023-10-12 20:58:56 -0300

    CLI - finalizado, config Go work realizada

--- projetos/cli/app/app.go
+++ projetos/cli/app/app.go
@@ -21,21 +21,53 @@ func Gerar() *cli.App {
 	app.Name = "Primeira CLI feita em Go"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	flags := configurarFlags()
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na Internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "sptech.school",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na Internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca o nome dos servidores na Internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
 	return app
+}
+
+func configurarFlags() []cli.Flag {
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "sptech.school",
+		},
+	}
+
+	return flags
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host") // Busca o valor da flag host
+
+	fmt.Printf("Buscando servidores do host: %s\n", host)
+
+	// Pacote net
+	servidores, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal("Não foi possível encontrar o servidor do host informado\n", erro)
+	} else {
+		for index, servidor := range servidores {
+			fmt.Printf("Servidor[%d]: %s\n", (index + 1), servidor.Host)
+		}
+	}
 }
 
 func buscarIps(c *cli.Context) {
